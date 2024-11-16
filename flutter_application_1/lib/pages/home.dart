07e9b6715de90bb34c136699420660155006c9c3
Silvestri Ndoj <silvesterndoj13@gmail.com>
2024-11-16 18:30:01 +0100

    a

--- flutter_application_1/lib/pages/home.dart
+++ flutter_application_1/lib/pages/home.dart
@@ -44,8 +44,8 @@ class HomePage extends StatelessWidget {
           navigatorkey: _tab3navigatorKey,
         ),
       ],
-      selectedItemColor: Colors.blue, // Customize the selected item color
-      unselectedItemColor: Colors.grey, // Customize the unselected item color
+      selectedItemColor: Colors.red, // Customize the selected item color
+      unselectedItemColor: Colors.black, // Customize the unselected item color
       bottomBarBackgroundColor: Colors.white,
     );
   }
